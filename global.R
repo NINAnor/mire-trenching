344ad7b68a1a56c388352e105d4ad57df73149fa
Anders L. Kolstad <anders-lk@hotmail.com>
2024-08-08 13:12:52 +0200

    fix global

--- global.R
+++ global.R
@@ -10,3 +10,12 @@ lev <- sf::read_sf("data/lev.gpkg")
 lev_bb <- sf::st_bbox(lev)
 myx <- unname(round(lev_bb["xmax"] - lev_bb["xmin"]))
 myy <- unname(round(lev_bb["ymax"] - lev_bb["ymin"]))
+
+
+save(
+  #lev,
+  lev_bb,
+  myx,
+  myy,
+  file = "data/global.RData"
+     )
